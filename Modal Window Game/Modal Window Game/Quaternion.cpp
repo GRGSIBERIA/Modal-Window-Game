65686d4f187bb4c72b8aa72eec1d5dbc7f3aca04
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-04-13 22:04:05 +0900

    プロジェクトの準備と四元数の実装

---
+++ Modal Window Game/Modal Window Game/Quaternion.cpp
@@ -0,0 +1,59 @@
+﻿#include "Quaternion.h"
+using namespace mwg;
+
+inline Quaternion mwg::Quaternion::mul(const Quaternion & rhs) const
+{
+	return Quaternion(w * rhs.w - v.dot(rhs.v), w * rhs.v + v * rhs.w + v.cross(rhs.v));
+}
+
+mwg::Quaternion::Quaternion()
+	: v(0.0, 0.0, 0.0), w(0.0)
+{
+}
+
+mwg::Quaternion::Quaternion(double v1, double v2, double v3, double w)
+	: v(v1, v2, v3), w(w)
+{
+}
+
+mwg::Quaternion::Quaternion(const double w, const s3d::Vec3& v)
+	: v(v), w(w)
+{
+}
+
+mwg::Quaternion mwg::Quaternion::operator*(const Quaternion & rhs) const
+{
+	return mul(rhs).Normalize();
+}
+
+s3d::Vec3 mwg::Quaternion::operator*(const s3d::Vec3 & vec) const
+{
+	const auto q = mul(Quaternion(0.0, vec)).mul(Conjugate());
+	return q.v;
+}
+
+Quaternion mwg::Quaternion::Normalize() const
+{
+	auto q = Quaternion(w, v);
+	auto l = 1.0 / Length();
+	q.v *= l;
+	q.w *= l;
+	return q;
+}
+
+Quaternion mwg::Quaternion::Conjugate() const
+{
+	return Quaternion(w, -v);
+}
+
+double mwg::Quaternion::Length() const
+{
+	return s3d::Sqrt(v.x * v.x + v.y * v.y + v.z * v.z + w * w);
+}
+
+Quaternion mwg::Quaternion::AxisAngle(const s3d::Vec3 & axis, const double radian)
+{
+	const auto c = cos(radian * 0.5);
+	const auto s = sin(radian * 0.5);
+	return Quaternion(c, axis.normalized() * s).Normalize();
+}
---
+++ Modal Window Game/Modal Window Game/Quaternion.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+namespace mwg
+{
+	class Quaternion
+	{
+		s3d::Vec3 v;
+		double w;
+
+		inline Quaternion mul(const Quaternion& rhs) const;
+
+	public:
+		Quaternion();
+
+		Quaternion(double v1, double v2, double v3, double w);
+
+		Quaternion(const double w, const s3d::Vec3& vecor);
+
+		Quaternion operator*(const Quaternion& rhs) const;
+
+		s3d::Vec3 operator*(const s3d::Vec3& vec) const;
+
+		Quaternion Normalize() const;
+
+		Quaternion Conjugate() const;
+
+		double Length() const;
+
+		static Quaternion AxisAngle(const s3d::Vec3& axis, const double radian);
+	};
+}
