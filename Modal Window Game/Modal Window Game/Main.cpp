74955c005efe146630c5bc54ab97ff3194008624
grgsiberia <nanashi4129@gmail.com>
2019-04-14 16:58:05 +0900

    Hamframework使えなかった

--- Modal Window Game/Modal Window Game/Main.cpp
+++ Modal Window Game/Modal Window Game/Main.cpp
@@ -9,18 +9,9 @@ void Main()
 
 	const Font font(60);
 
-	const Texture textureCat(Emoji(U"🐈"), TextureDesc::Mipped);
-
 	Window::SetTitle(U"Breath of the modal");
 
 	while (System::Update())
 	{
-		font(U"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
-
-		font(Cursor::Pos()).draw(20, 500, ColorF(0.6));
-
-		textureCat.resized(80).draw(700, 500);
-
-		Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
 	}
 }
--- Modal Window Game/Modal Window Game/ToneArray.h
+++ Modal Window Game/Modal Window Game/ToneArray.h
@@ -5,7 +5,16 @@ namespace bom
 {
 	class ToneArray
 	{
-		std::array<double, 7> tones = { 0.0, 2.0, 4.0, 5.0, 7.0, 9.0, 11.0 };
+	protected:
+		const std::array<double, 7> tones = { 0.0, 2.0, 4.0, 5.0, 7.0, 9.0, 11.0 };
+
+	public:
+	};
+
+	class BasicTone
+	{
+	protected:
+		const std::array<double, 12> tones = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0 };
 
 	public:
 	};
